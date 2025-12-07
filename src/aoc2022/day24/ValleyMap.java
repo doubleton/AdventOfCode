2428635c807a2e3c9346091485fa267cc2760245
Anton Antonov <anton.o.antonov@gmail.com>
2025-12-07 12:10:32 +0400

    AOC 2022 - Day 24 part1

--- src/aoc2022/day24/ValleyMap.java
+++ src/aoc2022/day24/ValleyMap.java
@@ -1,35 +1,35 @@
 package aoc2022.day24;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
-
-import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class ValleyMap {
 
     private final Position entrance;
     private final Position exit;
-    private final ArrayListMultimap<Position, Direction> blizzards;
+    private final List<Blizzard> blizzards;
     private final int maxRow;
     private final int maxCol;
+    private final int cycleLength;
+    private final List<boolean[][]> blizzardStates;
 
-    public ValleyMap(Position entrance, Position exit, ArrayListMultimap<Position, Direction> blizzards, int maxRow, int maxCol) {
+    private ValleyMap(Position entrance, Position exit, List<Blizzard> blizzards, int maxRow, int maxCol) {
         this.entrance = entrance;
         this.exit = exit;
         this.blizzards = blizzards;
         this.maxRow = maxRow;
         this.maxCol = maxCol;
+        this.cycleLength = lcm(maxRow, maxCol);
+        this.blizzardStates = precomputeBlizzardStates();
     }
 
     public static ValleyMap init(List<String> input) {
-        ArrayListMultimap<Position, Direction> blizzards = ArrayListMultimap.create();
+        List<Blizzard> blizzards = new ArrayList<>();
         Position entrance = null, exit = null;
-        int maxRow = input.size() - 2, maxCol = input.get(0).length() - 2;
+        int maxRow = input.size() - 2, maxCol = input.getFirst().length() - 2;
 
         for (int row = 0; row < input.size(); row++) {
             char[] chars = input.get(row).toCharArray();
@@ -46,7 +46,7 @@ public class ValleyMap {
                     continue;
                 }
 
-                if (col >= 1 && col <= maxCol && value != '.') {
+                if (row >= 1 && row <= maxRow && col >= 1 && col <= maxCol) {
                     Direction direction = switch (value) {
                         case '^' -> Direction.UP;
                         case 'v' -> Direction.DOWN;
@@ -54,7 +54,9 @@ public class ValleyMap {
                         case '<' -> Direction.LEFT;
                         default -> null;
                     };
-                    blizzards.put(new Position(row, col), direction);
+                    if (direction != null) {
+                        blizzards.add(new Blizzard(row, col, direction));
+                    }
                 }
             }
         }
@@ -66,108 +68,109 @@ public class ValleyMap {
         LinkedList<QueueKey> queue = new LinkedList<>();
         queue.add(new QueueKey(0, entrance));
         Set<SeenKey> seen = new HashSet<>();
-        int lcd = maxRow * maxCol / gcd(maxRow, maxCol);
+        seen.add(new SeenKey(entrance, 0));
+
+        int[][] directions = new int[][] {
+                {0, 0},    // wait
+                {-1, 0},   // up
+                {1, 0},    // down
+                {0, -1},   // left
+                {0, 1}     // right
+        };
 
         while (!queue.isEmpty()) {
             QueueKey queueKey = queue.pollFirst();
             Position curPos = queueKey.position;
-            int move = queueKey.move + 1;
+            int nextMinute = queueKey.minute + 1;
+            int blizzardIndex = nextMinute % cycleLength;
+            boolean[][] blizzardsAtNextMinute = blizzardStates.get(blizzardIndex);
 
-            if (curPos.moveExpedition(Direction.DOWN, maxRow + 1, maxCol).equals(exit)) {
-                return move;
+            for (int[] dir : directions) {
+                int newRow = curPos.row() + dir[0];
+                int newCol = curPos.col() + dir[1];
+                Position newPos = new Position(newRow, newCol);
+
+                if (newPos.equals(exit)) {
+                    return nextMinute;
+                }
+
+                if (!isInside(newPos) && !newPos.equals(entrance)) {
+                    continue;
+                }
+
+                if (hasBlizzard(newPos, blizzardsAtNextMinute)) {
+                    continue;
+                }
+
+                SeenKey seenKey = new SeenKey(newPos, blizzardIndex);
+                if (seen.add(seenKey)) {
+                    queue.add(new QueueKey(nextMinute, newPos));
+                }
             }
+        }
+        throw new IllegalStateException("No path found");
+    }
 
-            ArrayListMultimap<Position, Direction> nextBlizzards = nextBlizzards(move);
+    private boolean hasBlizzard(Position pos, boolean[][] blizzardsAtMinute) {
+        if (pos.row() < 1 || pos.row() > maxRow || pos.col() < 1 || pos.col() > maxCol) {
+            return false;
+        }
+        return blizzardsAtMinute[pos.row()][pos.col()];
+    }
 
-            for (Direction direction : Direction.values()) {
-                Position newPos = curPos.moveExpedition(direction, maxRow, maxCol);
-                if (newPos != null && nextBlizzards.get(newPos).isEmpty()) {
-                    SeenKey seenKey = new SeenKey(newPos, move % lcd);
-                    if (!seen.contains(seenKey)) {
-                        seen.add(seenKey);
-                        queue.add(new QueueKey(move, newPos));
+    private boolean isInside(Position pos) {
+        return pos.row() >= 1 && pos.row() <= maxRow && pos.col() >= 1 && pos.col() <= maxCol;
+    }
+
+    private List<boolean[][]> precomputeBlizzardStates() {
+        List<boolean[][]> states = new ArrayList<>(cycleLength);
+
+        for (int minute = 0; minute < cycleLength; minute++) {
+            boolean[][] grid = new boolean[maxRow + 1][maxCol + 1];
+            for (Blizzard blizzard : blizzards) {
+                int row = blizzard.row();
+                int col = blizzard.col();
+                Direction direction = blizzard.direction();
+
+                int targetRow = row;
+                int targetCol = col;
+
+                switch (direction) {
+                    case UP -> targetRow = 1 + mod(row - 1 - minute, maxRow);
+                    case DOWN -> targetRow = 1 + mod(row - 1 + minute, maxRow);
+                    case LEFT -> targetCol = 1 + mod(col - 1 - minute, maxCol);
+                    case RIGHT -> targetCol = 1 + mod(col - 1 + minute, maxCol);
+                    case WAIT -> {
                     }
                 }
+
+                grid[targetRow][targetCol] = true;
             }
+            states.add(grid);
         }
-        throw new RuntimeException("Ouch");
+
+        return states;
     }
 
-    private ArrayListMultimap<Position, Direction> nextBlizzards(Multimap<Position, Direction> blizzards) {
-        ArrayListMultimap<Position, Direction> nextBlizzards = ArrayListMultimap.create();
-        for (Map.Entry<Position, Direction> entry : blizzards.entries()) {
-            Position position = entry.getKey();
-            Direction direction = entry.getValue();
-            if (direction != null) {
-                nextBlizzards.put(position.moveBlizzard(direction, maxRow, maxCol), direction);
-            }
-        }
-        return nextBlizzards;
+    private int mod(int value, int limit) {
+        int result = value % limit;
+        return result < 0 ? result + limit : result;
     }
 
-    private ArrayListMultimap<Position, Direction> nextBlizzards(int moves) {
-        ArrayListMultimap<Position, Direction> nextBlizzards = ArrayListMultimap.create();
-        for (Map.Entry<Position, Direction> entry : blizzards.entries()) {
-            Position position = entry.getKey();
-            Direction direction = entry.getValue();
-            if (direction != null) {
-                Position newPosition = position;
-                for (int i = 1; i <= moves; i++) {
-                    newPosition = newPosition.moveBlizzard(direction, maxRow, maxCol);
-                }
-                nextBlizzards.put(newPosition, direction);
-            }
-        }
-        return nextBlizzards;
-    }
-
-    public void print(Position curPos, ArrayListMultimap<Position, Direction> blizzards) {
-        System.out.println(blizzards.entries());
-
-        for (int row = 0; row <= maxRow + 1; row++) {
-            for (int col = 0; col <= maxCol + 1; col++) {
-                Position pos = new Position(row, col);
-                if (pos.equals(curPos)) {
-                    System.out.print('E');
-                } else if (pos.equals(exit)) {
-                    System.out.print('F');
-                } else if (row == 0 || row == maxRow + 1 || col == 0 || col == maxCol + 1) {
-                    System.out.print('#');
-                } else {
-                    List<Direction> blizzardsAtPos = blizzards.get(pos);
-                    if (blizzardsAtPos.isEmpty()) {
-                        System.out.print('.');
-                    } else if (blizzardsAtPos.size() == 1) {
-                        switch (blizzardsAtPos.get(0)) {
-                            case UP -> {
-                                System.out.print('^');
-                            }
-                            case DOWN -> {
-                                System.out.print('v');
-                            }
-                            case LEFT -> {
-                                System.out.print('<');
-                            }
-                            case RIGHT -> {
-                                System.out.print('>');
-                            }
-                        }
-                    } else {
-                        System.out.print(blizzardsAtPos.size());
-                    }
-                }
-            }
-            System.out.println();
-        }
+    private static int lcm(int a, int b) {
+        return a / gcd(a, b) * b;
     }
 
     private static int gcd(int a, int b) {
-        BigInteger b1 = BigInteger.valueOf(a);
-        BigInteger b2 = BigInteger.valueOf(b);
-        BigInteger gcd = b1.gcd(b2);
-        return gcd.intValue();
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 
-    private record QueueKey(int move, Position position) {}
-    private record SeenKey(Position position, int blizzardsHash) {}
+    private record Blizzard(int row, int col, Direction direction) {}
+    private record QueueKey(int minute, Position position) {}
+    private record SeenKey(Position position, int blizzardHash) {}
 }
